7e9873f59dcdfae255740619c8ec1df406654966
Daniel Yount <yountdaniel@gmail.com>
2019-01-18 23:32:49 -0500

    The ScrollView now can report back to the ViewController which subview is in focus.

--- PillzCoffeeOrder/CoffeeOrder.swift
+++ PillzCoffeeOrder/CoffeeOrder.swift
@@ -10,10 +10,17 @@ import Foundation
 import UIKit
 
 struct CoffeeOrder {
+    
     var title: String?
     var subtitle: String?
     var image: UIImage?
+    var color: UIColor?
     
-    
+    init(title: String, subtitle: String, image: UIImage, color: UIColor) {
+        self.title = title
+        self.subtitle = subtitle
+        self.image = image
+        self.color = color
+    }
     
 }
--- PillzCoffeeOrder/CoffeeOrderSelectorViewController.swift
+++ PillzCoffeeOrder/CoffeeOrderSelectorViewController.swift
@@ -11,26 +11,28 @@ import UIKit
 class CoffeeOrderSelectorViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var coffeeHeroScrollView: UIScrollView!
+    @IBOutlet weak var coffeeTitle: UILabel!
+    @IBOutlet weak var coffeeSubtitle: UILabel!
     
     var currentItemInFocus: CoffeeOrder?
+    var coffeeOrders: [CoffeeOrder] = [CoffeeOrder]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        coffeeOrders = createCoffeeOrders()
         setupScrollView()
         
-        let view1 = UIView()
-        view1.frame = CGRect(x: view.frame.width * 0, y: 0, width: coffeeHeroScrollView.frame.width, height: coffeeHeroScrollView.frame.height)
-        view1.backgroundColor = .red
-        let view2 = UIView()
-        view2.frame = CGRect(x: view.frame.width * 1, y: 0, width: coffeeHeroScrollView.frame.width, height: coffeeHeroScrollView.frame.height)
-        view2.backgroundColor = .green
+        var counter = 0
         
-        print(view1.frame)
-        print(view2.frame)
-        
-        coffeeHeroScrollView.addSubview(view1)
-        coffeeHeroScrollView.addSubview(view2)
+        for coffee in coffeeOrders {
+            
+            let view = UIImageView(frame: CGRect(x: coffeeHeroScrollView.frame.width * CGFloat(counter), y: 0, width: coffeeHeroScrollView.frame.width, height: coffeeHeroScrollView.frame.height))
+            view.image = coffee.image
+            view.contentMode = .scaleAspectFit
+            self.coffeeHeroScrollView.addSubview(view)
+            counter += 1
+        }
         
     }
     
@@ -41,12 +43,48 @@ class CoffeeOrderSelectorViewController: UIViewController, UIScrollViewDelegate 
         coffeeHeroScrollView.isPagingEnabled = true
         coffeeHeroScrollView.isScrollEnabled = true
         
-        coffeeHeroScrollView.contentSize = CGSize(width: coffeeHeroScrollView.frame.width * 2, height: coffeeHeroScrollView.frame.height)
+        let width = coffeeHeroScrollView.frame.width * CGFloat(coffeeOrders.count)
+        let height = coffeeHeroScrollView.frame.height
+        
+        coffeeHeroScrollView.contentSize = CGSize(width: width, height: height)
         
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var counter = 0
+        for subview in scrollView.subviews {
+            
+//            print("subview number \(counter) has a midX point of \(subview.frame.midX) which we are comparing to \(scrollView.bounds.midX)")
+            
+            if (subview.frame.midX == scrollView.bounds.midX) {
+                print("The item in focus is \(counter)")
+                let coffee = coffeeOrders[counter]
+                self.coffeeTitle.text = coffee.title
+                self.coffeeSubtitle.text = coffee.subtitle
+                self.view.backgroundColor = coffee.color
+            }
+            
+            counter += 1
+            
+        }
         
+    }
+    
+    func createCoffeeOrders() -> [CoffeeOrder] {
+        
+        var result = [CoffeeOrder]()
+        
+        let coffeeOrder1 = CoffeeOrder(title: "Mint Mojito", subtitle: "Is it Mint Mojito time?", image: #imageLiteral(resourceName: "mintMojito"), color: UIColor(red:0.32, green:0.90, blue:0.56, alpha:1.0))
+        
+        let coffeeOrder2 = CoffeeOrder(title: "Silken Splendor", subtitle: "In search of a Silken Splendor?", image: #imageLiteral(resourceName: "silkenSplendor"), color: UIColor(red:1.00, green:0.78, blue:0.25, alpha:1.0))
+        
+        let coffeeOrder3 = CoffeeOrder(title: "Tesora", subtitle: "Got time for our classic Tesora?", image: #imageLiteral(resourceName: "tesora"), color: UIColor(red:1.00, green:0.66, blue:0.00, alpha:1.0))
+        
+        result.append(coffeeOrder1)
+        result.append(coffeeOrder2)
+        result.append(coffeeOrder3)
+        
+        return result
     }
 
 
